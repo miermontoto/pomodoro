269a8b1b669f9f032a8f3d1ab2656a5307ac4f6f
miermontoto <UO283319@uniovi.es>
2023-09-05 20:50:26 +0200

    add footer

--- src/App.vue
+++ src/App.vue
@@ -7,4 +7,17 @@
 <template>
 	<MarqueeComponent />
 	<TimerComponent />
+
+	<div id="footer">
+		<a href="https://mier.info">
+			<img src="https://mier.info/assets/favicon.svg" id="footer-logo" alt="logo">
+		</a>
+		<span id="footer-text">
+			made
+			with <span class="heart">❤</span>
+			for <a href="https://okticket.es"><img src="https://mier.info/assets/logos/okticket.png" width="256" height="256" id="oktlogo" alt="okticket"></a>
+			by <a href="https://mier.info">Juan Mier</a>
+			in <code>Vue.js 3</code>
+		</span>
+	</div>
 </template>
